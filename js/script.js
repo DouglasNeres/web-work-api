2450d4875b859fcd3a337b15f9bc489a257582cb
DouglasNeres <douglasneres06@gmail.com>
2022-11-28 10:57:22 -0300

    WW - Upgrade in page, with button of inversion

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 async function convert() {
   let button = document.getElementById("btn");
   let retry = document.getElementById("clean");
+  let invert = document.getElementById("invert");
   let request = document.getElementById("value");
   let converted = document.getElementById("result");
+  
+  const coin1 = document.getElementById("coin1");
+  const coin2 = document.getElementById("coin2");
 
   button.addEventListener("click", async (e) => {
     e.preventDefault();
 
-    const coin1 = document.getElementById("coin1").value;
-    const coin2 = document.getElementById("coin2").value;
-
-    const api = `https://economia.awesomeapi.com.br/last/${coin1}-${coin2}`;
+    const fieldConvert = coin1.value;
+    const fieldFor = coin2.value;
+    
+    const api = `https://economia.awesomeapi.com.br/last/${fieldConvert}-${fieldFor}`;
     const response = await (await fetch(api)).json();
 
     const req = request.value;
 
-    const result = response[`${coin1}${coin2}`].high;
+    const result = response[`${fieldConvert}${fieldFor}`].high;
 
     let calc = (req * result).toFixed(2);
     
@@ -28,6 +32,16 @@ async function convert() {
     converted.value = ""
   })
 
+  invert.addEventListener('click',  async (e) => {
+      e.preventDefault()
+
+    const fieldConvert = coin1.value
+    const fieldFor = coin2.value
+
+    coin1.value = fieldFor
+    coin2.value = fieldConvert
+  
+    })
 
 }
 
