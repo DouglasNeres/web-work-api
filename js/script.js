03f5b0417f7fb5fe583ff3c93ec39d30a5b59354
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2022-12-01 14:35:19 -0300

    WW - JavaScript Refactor

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ async function convert() {
   let button = document.getElementById("btn");
   let retry = document.getElementById("clean");
   let invert = document.getElementById("invert");
+
   let request = document.getElementById("value");
   let converted = document.getElementById("result");
-  
+
   const coin1 = document.getElementById("coin1");
   const coin2 = document.getElementById("coin2");
 
@@ -13,36 +14,36 @@ async function convert() {
 
     const fieldConvert = coin1.value;
     const fieldFor = coin2.value;
-    
+    const req = request.value;
+
+    if (fieldConvert === fieldFor) alert("Escolha moedas diferentes");
+    if (req === "") alert("Digite algo no Campo");
+
     const api = `https://economia.awesomeapi.com.br/last/${fieldConvert}-${fieldFor}`;
     const response = await (await fetch(api)).json();
-
-    const req = request.value;
 
     const result = response[`${fieldConvert}${fieldFor}`].high;
 
     let calc = (req * result).toFixed(2);
-    
-    converted.value = calc
+
+    converted.value = calc;
   });
 
-  retry.addEventListener('click', (e) => {
-    e.preventDefault()
-    request.value = ""
-    converted.value = ""
-  })
+  retry.addEventListener("click", (e) => {
+    e.preventDefault();
+    request.value = "";
+    converted.value = "";
+  });
 
-  invert.addEventListener('click',  async (e) => {
-      e.preventDefault()
+  invert.addEventListener("click", async (e) => {
+    e.preventDefault();
 
-    const fieldConvert = coin1.value
-    const fieldFor = coin2.value
+    const fieldConvert = coin1.value;
+    const fieldFor = coin2.value;
 
-    coin1.value = fieldFor
-    coin2.value = fieldConvert
-  
-    })
-
+    coin1.value = fieldFor;
+    coin2.value = fieldConvert;
+  });
 }
 
 convert();
